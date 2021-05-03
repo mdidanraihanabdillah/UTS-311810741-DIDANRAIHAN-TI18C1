a15232d6931e47d3441942058b1ed0d54e8d5181
mdidanraihanabdillah <mandorbeek@gmail.com>
2021-05-03 19:29:20 +0700

    project

---
+++ app/src/main/java/com/didan/uts/MainActivity.java
@@ -0,0 +1,41 @@
+package com.affan.uts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
+import android.os.Bundle;
+import com.google.android.material.tabs.TabLayout;
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        //Menerapkan TabLayout dan ViewPager pada Activity
+        final TabLayout tabLayout = findViewById(R.id.tab_layout);
+        final ViewPager viewPager = findViewById(R.id.pager);
+        //Memanggil dan Memasukan Value pada Class PagerAdapter(FragmentManager dan JumlahTab)
+        PagerAdapter pagerAdapter = new PagerAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
+        //Memasang Adapter pada ViewPager
+        viewPager.setAdapter(pagerAdapter);
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                //Dipanggil ketika tab memasuki state/keadaan yang dipilih.
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+                //Dipanggil saat tab keluar dari keadaan yang dipilih.
+            }
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+                //Dipanggil ketika tab yang sudah dipilih, dipilih lagi oleh user.
+            }
+        });
+    }
+}
